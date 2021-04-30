d13be1d39e5653a3ec9b7b50c82e36af5fe3c1f5
Valentin Roussellet <vroussellet@apple.com>
2021-04-29 21:24:25 -0700

    animate rendering

--- pendulums.cpp
+++ pendulums.cpp
@@ -14,6 +14,7 @@
 #include <stb_image_write.h>
 
 // Decide on floating point type
+#define USE_DOUBLE
 #ifdef USE_DOUBLE
 typedef double Scalar;
 #else
@@ -52,6 +53,7 @@ struct PendulumSim
 
     PendulumSim()
     {
+        // Precompute stuff
         for (int i = 0; i < K; ++i)
         {
             const Scalar angle = 2 * Scalar(i) * Pi / Scalar(K);
@@ -76,7 +78,7 @@ struct PendulumSim
         }
 
         // Semi-implicit Euler
-        // Since g = 1 gravity = -pos
+        // Since m = 1 g = 1 gravity = -pos
         vel += dt * (magnetic - pos);
         vel *= friction_coeff;
         pos += dt * vel;
@@ -84,19 +86,36 @@ struct PendulumSim
 };
 
 // What we want to compute : an image which represents the attractor basins of the magnetic pendulum
-// N = image width ( & height)
+// N = image width (& height)
 template <uint N>
 struct Image
 {
-    const Scalar extents = 4.0_f;               // Half width of the plane represented by the image
-    const int channels = 3;                     // RGB image
-    const int pixel_count = N * N;              // How many pixels ?
-    const size_t size = pixel_count * channels; // Data size in memory
+    static const int width = N; // Image dimensions
+    static const int height = N;
+    static const int channels = 3;                     // RGB image
+    static const int pixel_count = width * height;     // How many pixels ?
+    static const size_t size = pixel_count * channels; // Data size in memory
 
-    uchar *buffer; // Dynamically allocated memory for pixels
+    Vec2 center = Vec2{0, 0}; // What point in the plane we center on
+    Scalar extents = 4.0_f;   // Half width of the plane represented by the image
+    uchar *buffer = nullptr;  // Dynamically allocated memory for pixels
 
-    Image() : buffer(new uchar[size])
+    Image()
+        : buffer(new uchar[size])
     {
+        memset(buffer, 0, size);
+    }
+
+    Image(const Vec2 &c, Scalar e)
+        : extents(e), center(c), buffer(new uchar[size])
+    {
+        memset(buffer, 0, size);
+    }
+
+    void reset(const Vec2 &c, Scalar e)
+    {
+        center = c;
+        extents = e;
         memset(buffer, 0, size);
     }
 
@@ -107,7 +126,7 @@ struct Image
 
     // Compute the color at pixel #index
     template <uint K>
-    void simulate(const PendulumSim<K> &sim, int index)
+    void simulate(const PendulumSim<K> *sim, int index)
     {
 
         CORE_ASSERT(index < pixel_count, "Wrong index");
@@ -120,7 +139,7 @@ struct Image
         const Scalar x = (xi + 0.5_f) / N;
         const Scalar y = (yi + 0.5_f) / N;
 
-        Vec2 pos = extents * (2 * Vec2{x, y} - Vec2::Ones());
+        Vec2 pos = extents * (2 * Vec2{x, y} - Vec2::Ones()) + center;
         Vec2 vel = Vec2::Zero();
 
         ON_DEBUG(std::cout << "Sim " << index << "|" << xi << " " << yi << "|" << pos.transpose());
@@ -130,18 +149,18 @@ struct Image
         int iter = 0;
         do
         {
-            sim.update(pos, vel);
+            sim->update(pos, vel);
             ++iter;
-        } while (vel.squaredNorm() > sim.vel_epsilon && iter < max_iters);
+        } while (vel.squaredNorm() > sim->vel_epsilon && iter < max_iters);
 
         ON_DEBUG(std::cout << " /  " << iter << " " << pos.transpose() << " " << vel.squaredNorm() << std::endl);
 
         // Choose color depending on nearest magnet
         int nearest = 0;
         Scalar d2_min = FLT_MAX;
-        for (int i = 0; i < sim.num_magnets; ++i)
+        for (int i = 0; i < sim->num_magnets; ++i)
         {
-            const Scalar d2 = (pos - sim.magnet_positions.col(i)).squaredNorm();
+            const Scalar d2 = (pos - sim->magnet_positions.col(i)).squaredNorm();
             if (d2 < d2_min)
             {
                 nearest = i;
@@ -168,50 +187,76 @@ struct Image
     {
         stbi_write_png(filename, N, N, channels, buffer, N * channels);
     }
+
+    // Render a full sim to an image, potentially on multiple threads
+    template <uint K>
+    void render(const PendulumSim<K> *sim, const char *filename)
+    {
+        const auto start = std::chrono::high_resolution_clock::now();
+
+#ifdef SINGLE_THREADED
+        const uint thread_count = 1;
+        for (int i = 0; i < pixel_count; ++i)
+        {
+            simulate(sim, i);
+        }
+#else
+        const uint thread_count = std::thread::hardware_concurrency();
+        std::vector<std::thread> threads;
+        threads.reserve(thread_count);
+
+        for (uint t = 0; t < thread_count; ++t)
+        {
+            threads.emplace_back(
+                [sim, this, t, thread_count] {
+                    const int start = pixel_count * t / thread_count;
+                    const int stop = std::min<int>(pixel_count, pixel_count * (t + 1) / thread_count);
+                    for (int i = start; i < stop; ++i)
+                    {
+                        simulate(sim, i);
+                    }
+                });
+        }
+        for (auto &t : threads)
+        {
+            t.join();
+        }
+#endif
+        const auto end = std::chrono::high_resolution_clock::now();
+        const uint64 us = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+        std::cout << filename << ": " << us / 1000.0 << " ms  on " << thread_count << " threads (" << us / (pixel_count) << " us/pixel)" << std::endl;
+        save(filename);
+    }
 };
 
 int main()
 {
+    const Vec2 start = {0, 0};
+    const Vec2 target = {1.325, 1.480};
+    const Scalar start_ext = 4.0_f;
+    const Scalar target_ext = 0.01_f;
+
     PendulumSim<3> sim;
-    Image<512> img;
+    Image<1080> img;
 
-    const auto start = std::chrono::high_resolution_clock::now();
-
-#ifdef SINGLE_THREADED
-    const uint thread_count = 1;
-    for (int i = 0; i < img.pixel_count; ++i)
+    const int num_frames = 30 * 20;
+    // Compute a full animation
+    for (int i = 0; i < num_frames; ++i)
     {
-        img.simulate(sim, i);
+        const Scalar a = Scalar(i) / (num_frames - 1);
+
+        const Scalar tPos = std::min<Scalar>(2 * a, 1); // Reach target position halfway through
+        const Vec2 center = tPos * target + (1 - tPos) * start;
+
+        const Scalar tZoom = a * a * (3 - 2 * a); // smoothstep
+        const Scalar zoom_level = tZoom * log(target_ext) + (1 - tZoom) * log(start_ext);
+        const Scalar ext = exp(zoom_level);
+
+        img.reset(center, ext);
+        std::string name;
+        core::stringPrintf(name, "frame%04d.png", i);
+        img.render(&sim, name.c_str());
     }
-#else
-    const uint thread_count = std::thread::hardware_concurrency();
-    std::vector<std::thread> threads;
-    threads.reserve(thread_count);
-
-    for (uint t = 0; t < thread_count; ++t)
-    {
-        threads.emplace_back(
-            [t, thread_count, sim, &img] {
-                const int start = img.pixel_count * t / thread_count;
-                const int stop = std::min<int>(img.pixel_count, img.pixel_count * (t + 1) / thread_count);
-                for (int i = start; i < stop; ++i)
-                {
-                    img.simulate(sim, i);
-                }
-            });
-    }
-    for (auto &t : threads)
-    {
-        t.join();
-    }
-#endif
-
-    const auto end = std::chrono::high_resolution_clock::now();
-    const uint64 us = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-
-    std::cout << us / 1000.0 << " ms  on " << thread_count << " threads (" << us / (img.pixel_count) << " us/pixel)" << std::endl;
-
-    img.save("test.png");
-
     return 0;
 }
--- python/pendulum.py
+++ python/pendulum.py
@@ -9,10 +9,10 @@ num_magnets = 3         # How many magnets
 magnet_radius = 1.0     # Circle on which magnets are positioned
 extents = 3.0           # Simulation drawing extents
 
-magnetic_exponent = 4       # n such as magnetic force = K/(r^n )
+magnetic_exponent = 4     # n such as magnetic force = K/(r^n )
 magnetic_constant = 1     # multiplicative constant of magnetic force (in natural units)
-friction = 0.1             # dissipative term
-height = 0.5
+friction = 0.1            # dissipative term
+height = 0.5              # How high is the pendulum from the magnet
 delta_t = 0.01
 
 magnet_positions = np.array([np.array([
@@ -20,7 +20,6 @@ magnet_positions = np.array([np.array([
     magnet_radius * np.sin(2 * i * np.pi / num_magnets)])
     for i in range(num_magnets)])
 
-#print(magnet_positions)
 
 
 def update(pos, vel, dt):
@@ -60,43 +59,50 @@ def simulate(starting_pos, starting_vel, dt, epsilon = 1e-7):
 
     return trajectory
 
+pos1 = np.array([2.0,0.8])
+vel1 = np.zeros(2)
+traj1 = np.array([pos1])
 
-fig = plt.figure()
-ax = plt.axes(xlim = [-extents, extents], ylim = [-extents, extents])
-
-ax.scatter(x= magnet_positions[:,0], y = magnet_positions[:,1])
-
-
-pos = np.array([1.0,1.0])
-vel = np.zeros(2)
-traj = np.array([pos])
-points, = ax.plot([], [], "r,", ms=1)
+pos2 = np.array([2.02,0.8])
+vel2 = np.zeros(2)
+traj2 = np.array([pos2])
+points1, = ax.plot([], [], "r,-", ms=1)
+points2, = ax.plot([], [], "k,-", ms=1)
 
 def init():
-    points.set_data([],[])
-    points.set_marker(',')
-    points.set_color('r')
-    return points,
+    points1.set_data([],[])
+    points1.set_marker(',')
+    points1.set_color('r')
 
+    points2.set_data([],[])
+    points2.set_marker(',')
+    points2.set_color('k')
+    return points1,points2
+
+
+fig = plt.figure(figsize=(10,10))
+ax = plt.axes(xlim = [-extents, extents], ylim = [-extents, extents])
+ax.scatter(x= magnet_positions[:,0], y = magnet_positions[:,1])
 def animate(i):
-    global pos,vel,traj
-    ax.scatter(pos[0], pos[1])
-    pos,vel = update(pos,vel, delta_t)
+    global pos1,vel1,traj1
+    global pos2,vel2,traj2
+    #ax.scatter(pos[0], pos[1])
+    pos1,vel1 = update(pos1,vel1, delta_t)
+    pos2,vel2 = update(pos2,vel2, delta_t)
 
-    traj = np.append(traj, pos.reshape(1,2), 0)
+    traj1 = np.append(traj1, pos1.reshape(1,2), 0)
+    traj2 = np.append(traj2, pos2.reshape(1,2), 0)
 
-    if (np.linalg.norm(vel) < 1e-3):
+    # reset animation from a random point
+    if (np.linalg.norm(vel) < 1e-2):
         pos = 4 * (np.random.random_sample((2,)) -  0.5* np.ones((2,)))
         vel = np.zeros(2)
         traj = np.array([pos])
     
+    points1.set_data(traj1[:,0], traj1[:,1])
+    points2.set_data(traj2[:,0], traj2[:,1])
+    return points1,points2
 
 
-    points.set_data(traj[:,0], traj[:,1])
-    points.set_marker(',')
-    points.set_color('r')
-    return points,
-
-anim = animation.FuncAnimation(fig, animate, frames=1000, blit = True, interval = 100, init_func=init)
-
+anim = animation.FuncAnimation(fig, animate, frames=2000, blit = True, interval = 10, init_func=init)
 plt.show()
